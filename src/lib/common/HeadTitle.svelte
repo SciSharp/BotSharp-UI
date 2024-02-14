1fd47f99c2963833ee167cadd18350c23a5724d8
Jicheng Lu <franklujc@gmail.com>
2024-02-14 11:44:49 -0600

    refine head

--- src/lib/common/HeadTitle.svelte
+++ src/lib/common/HeadTitle.svelte
@@ -1,9 +1,10 @@
 <script>
 	import {PUBLIC_FAVICON_URL, PUBLIC_BRAND_NAME} from '$env/static/public'
 	export let title = 'Home';
+	export let addOn = 'Admin & Dashboard';
 </script>
 
 <svelte:head>
 	<link rel="icon" type="image/x-icon" href={PUBLIC_FAVICON_URL}>
-	<title>{title} | {PUBLIC_BRAND_NAME} - Admin & Dashboard</title>
+	<title>{title} | {PUBLIC_BRAND_NAME}{`${!!addOn ? ` - ${addOn}` : ''}`}</title>
 </svelte:head>
